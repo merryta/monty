572f863d6f83311e7df1bad6a8d9d72fd3f42aec
merryta <merryta251@gmail.com>
2021-05-13 17:38:01 +0000

    header of the file

---
+++ monty.h
@@ -0,0 +1,29 @@
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: point to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list nod structure
+ * for stack, ques, LIFO, FIFO HOlberton project
+ */
+typedef struct stack_s
+{
+  int n;
+  struct stack_s *prev;
+  struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Descripion: opcode an its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+  char *opcode;
+  void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
